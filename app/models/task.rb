4b64e713f526dddfb18a71bd7d15410422d9d94c
Minwoo Kim <mwkim0919@gmail.com>
2016-04-20 15:39:12 -0700

    removed datetime field in search. Added search msg.

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,12 +4,9 @@ class Task < ActiveRecord::Base
    validates :user_id, presence: true
    validates :title, presence: true, length: { maximum: 140 }
 
-   def self.search(title, start, finish)
-		# where("title LIKE ?", "%#{title}%")
-      # where("description LIKE ?", "%#{title}%")
-		# where("start = ?", start)
-		# where("finish = ?", finish)
-      where("title LIKE ? OR description LIKE ? OR start = ? OR finish = ?", "%#{title}%", "%#{title}%", start, finish)
+   def self.search(title)
+		where("title LIKE ?", "%#{title}%") ||
+      where("description LIKE ?", "%#{title}%")
    end
 
 end
