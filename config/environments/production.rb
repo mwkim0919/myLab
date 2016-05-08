2084090e113875fc3ceabcf43fc7b370f0b65c40
Minwoo Kim <mwkim0919@gmail.com>
2016-05-07 23:42:44 -0700

    some config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,5 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
